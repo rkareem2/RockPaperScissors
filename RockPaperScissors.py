d2384fd315703bd415c626f1b04a560ab397b67d
rkareem2 <shilerash12@gmail.com>
2024-03-21 14:41:29 -0400

    Second commit of the project after writing the codes. Now I have to review the logic and make sure it works as intended

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,3 +1,4 @@
+import random
 # New rock paper scissors project
 """
  In this game, the user gets the first chance to pick the option between Rock, paper, and scissors.
@@ -6,13 +7,80 @@
 
 # I would firstly ask both users how many rounds they wish to play
 x = 0
+y = 0
+userPoint = 0
+compPoint = 0
 rounds = int(input("How many rounds of the game ? "))
 maxPoint = int(input("What is the max point of the game? "))
 
-while x != rounds:
+while x != rounds or y != maxPoint:
     print(" Pick a choice \n 1 - Rock \n 2 - Paper \n 3 - Scissors")
     choice = int(input("Enter your choice"))
 
     # now we have to make sure user can only enter number of range 1 - 3
     while choice > 3 or choice < 1:
         choice = int(input("Please enter a valid choice \n 1 - Rock \n 2 - Paper \n 3 - Scissors"))
+
+    if choice == 1:
+        choiceName = "Rock"
+    elif choice == 2:
+        choiceName = "Paper"
+    else:
+        choiceName = "Scissors"
+
+    print("User has selected ", choiceName)
+
+    # Since only two users which is one user and the computer gets to choose randomly between 1,2,3
+    print("Now the computer has to make a selection")
+
+    # we give the computer a choice of picking between 3 random integers
+    choiceCom = random.randint(1,3)
+
+    # in rock paper scissors game both players cannot have the same value , so we make sure to avoid that
+    while choiceCom == choice:
+        choiceCom = random.randint(1, 3)
+
+    if choiceCom == 1:
+        choiceNameCom = "Rock"
+    elif choiceCom == 2:
+        choiceNameCom = "Paper"
+    else:
+        choiceNameCom = "Scissors"
+
+    print("Computer has selected ", choiceNameCom)
+# we put in a condition if it is a draw
+    if choice == choiceCom:
+        print("This is a draw")
+
+    # conditions for winning the game
+    if choice == 1 and choiceCom == 2:
+        compPoint = compPoint + 2
+        print("Computer wins ")
+    elif choice == 2 and choiceCom == 1:
+        userPoint = userPoint + 2
+        print("User wins")
+    elif choice == 1 and choiceCom == 3:
+        userPoint = userPoint + 2
+        print("User wins")
+    elif choice == 3 and choiceCom == 1:
+        compPoint = compPoint + 2
+        print("Computer wins")
+    elif choice == 2 and choiceCom == 3:
+        compPoint = compPoint + 2
+        print("Computer wins")
+    else:
+        userPoint = userPoint + 2
+        print("User wins")
+
+    if userPoint or compPoint == maxPoint:
+        print("You have reached the maximum point possible")
+        break
+
+    print("The user point is ", userPoint)
+    print("The Computer point is ", compPoint)
+
+    x = x + 1
+    y = y + 2
+
+print("Thanks for playing")
+
