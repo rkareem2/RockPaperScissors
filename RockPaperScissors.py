94504f0007f3c6630b0b1573c412659eeb8624f7
rkareem2 <shilerash12@gmail.com>
2024-03-23 14:42:01 -0400

    Third commit and my code is very close to the way I intended it to work but just a little for fine-tuning

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -4,16 +4,12 @@ import random
  In this game, the user gets the first chance to pick the option between Rock, paper, and scissors.
  After the computer select from the remaining two choices(randomly), the winner is decided as per the rules.
 """
-
-# I would firstly ask both users how many rounds they wish to play
-x = 0
 y = 0
 userPoint = 0
 compPoint = 0
-rounds = int(input("How many rounds of the game ? "))
 maxPoint = int(input("What is the max point of the game? "))
 
-while x != rounds or y != maxPoint:
+while y != maxPoint:
     print(" Pick a choice \n 1 - Rock \n 2 - Paper \n 3 - Scissors")
     choice = int(input("Enter your choice "))
 
@@ -49,15 +45,17 @@ while x != rounds or y != maxPoint:
 
     print("Computer has selected ", choiceNameCom)
     # conditions for winning the game
+    # conditions if the computer wins and it adds two points
     if (choice == 1 and choiceCom == 2) or (choice == 2 and choiceCom == 3) or (choice == 3 and choiceCom == 1):
-        compPoint += 1
+        compPoint += 2
         print("Computer wins!")
+    # conditions if user wins, and it adds 2 points to user point
     elif (choice == 1 and choiceCom == 3) or (choice == 2 and choiceCom == 1) or (choice == 3 and choiceCom == 2):
-        userPoint += 1
+        userPoint += 2
         print("User wins!")
     else:
         print("It's a draw!")
-
+    # we make an if statement that the user or computer reaches the maximum point it breaks the loop
     if userPoint == maxPoint or compPoint == maxPoint:
         print("You have reached the maximum point possible")
         break
@@ -65,8 +63,8 @@ while x != rounds or y != maxPoint:
     print("The user point is ", userPoint)
     print("The Computer point is ", compPoint)
 
-    x = x + 1
-    y = y + 2
-
+y = y + 2
+print("Total user point is ", userPoint)
+print("Total computer point is ", compPoint)
 print("Thanks for playing")
 
